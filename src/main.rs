8341b0532b25d61ad250986df3a8b200b017fe11
rkappes <rkappes18@gmail.com>
2025-02-19 12:52:45 -0800

    code to create interval and new note from interval

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 extern crate rust_music_theory as rustmt;
-use rustmt::note::{Note, Notes, PitchClass};
+use rustmt::note::{Note,Pitch, NoteLetter};
+use rustmt::interval::{Interval,Number, Quality};
 extern crate rodio;
 use std::error::Error;
 
 
-// fn main() {
-//     let note = Note::new(PitchClass::A, 4);
+fn main() {
+    let pitch = Pitch::new(NoteLetter::A,0);
+    let note = Note::new(pitch, 4);
+    let interval = Interval::new(3,Quality::Minor, Number::Third, None);
 
-//     println!("Note is {}", note);
+    println!("Note is {}", note);
+    println!("Interval is {}", interval);
 
-// }
+    let new_note = interval.second_note_from(note);
 
+    println!("new_note is {}", new_note);
+
+}
+
+/* 
  //Taken from: https://github.com/RustAudio/rodio/blob/master/examples/signal_generator.rs
 fn main() -> Result<(), Box<dyn Error>> {
     use rodio::source::{chirp, Function, SignalGenerator, Source};
@@ -34,3 +43,4 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+*/
