ad7ea4dc824a6d3c55894d58d4493c36d50f77e3
rkappes <rkappes18@gmail.com>
2025-02-24 21:20:33 -0800

    create module for playing a note

--- src/lib.rs
+++ src/lib.rs
@@ -139,3 +139,32 @@ pub mod intervals {
         note
     }
 }
+
+pub mod play {
+    extern crate rodio;
+    use std::error::Error;
+
+    //Taken from: https://github.com/RustAudio/rodio/blob/master/examples/signal_generator.rs with some minor changes
+    fn play_note(freq: f32) -> Result<(), Box<dyn Error>> {
+        use rodio::source::{chirp, Function, SignalGenerator, Source};
+        use std::thread;
+        use std::time::Duration;
+
+        let (_stream, stream_handle) = rodio::OutputStream::try_default()?; //Builder::open_default_stream()?;
+
+        let test_signal_duration = Duration::from_millis(1000);
+        let interval_duration = Duration::from_millis(1500);
+        let sample_rate = rodio::cpal::SampleRate(48000);
+
+        println!("Playing 440 Hz tone");
+        stream_handle.play_raw(
+            SignalGenerator::new(sample_rate, 440.0, Function::Sine)
+                .amplify(0.1)
+                .take_duration(test_signal_duration),
+        )?;
+
+        thread::sleep(interval_duration);
+
+        Ok(())
+    }
+}
