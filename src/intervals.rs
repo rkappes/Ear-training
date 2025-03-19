14def429a1dae3bd45bf24d6de4d62e56d020985
rkappes <rkappes18@gmail.com>
2025-03-19 15:43:16 -0700

    1) change regex to limit P, A, D to 4ths and 5th 2) include d4 and D4 as match options in create_interval_string 3) unit tests

--- src/intervals.rs
+++ src/intervals.rs
@@ -20,7 +20,7 @@ fn rand_interval() -> Number {
 */
 
 lazy_static! {
-    static ref REGEX_INTERVAL: Regex = Regex::new("^[MmPpAaDd][234567]").unwrap();
+    static ref REGEX_INTERVAL: Regex = Regex::new("^[Mm][2367]$|^[PpAaDd][45]$").unwrap();
 }
 
 pub fn check_if_interval(input: &str)->bool{
@@ -82,7 +82,7 @@ pub fn create_interval_string(interval: &str) -> Interval{
         "m2" => semitones = 1,
         "M2" => semitones =2,
         "m3" => semitones =3,
-        "M3" => semitones =4,
+        "M3" | "d4" | "D4" => semitones =4,
         "P4" | "p4" => semitones =5,
         "A4" | "a4" | "d5" | "D5" => semitones = 6,
         "P5" | "p5" => semitones = 7,
@@ -145,4 +145,108 @@ pub fn create_note_from_given_interval(root: Note, interval: Interval, direction
     };
     note
 }
-//}
+
+
+#[cfg(test)]
+mod tests {
+    use rust_music_theory::note::{Pitch,NoteLetter};
+
+    use super::*;
+
+    #[test]
+    fn test_check_if_interval_invalid_input(){
+        assert_eq!(false, check_if_interval("p3"));
+        assert_eq!(false, check_if_interval("M4"));
+        assert_eq!(false, check_if_interval("5"));
+        assert_eq!(false, check_if_interval("p8"));
+        assert_eq!(false, check_if_interval("h4"));
+        assert_eq!(false, check_if_interval("m2 p4"));
+        assert_eq!(false, check_if_interval("d7"));
+    }
+
+    #[test]
+    fn test_check_if_interval_valid_input(){
+        assert_eq!(true, check_if_interval("m2"));
+        assert_eq!(true, check_if_interval("P4"));
+        assert_eq!(true, check_if_interval("D5"));
+        assert_eq!(true, check_if_interval("a4"));
+        assert_eq!(true, check_if_interval("M7"));
+        assert_eq!(true, check_if_interval("d5"));
+        assert_eq!(true, check_if_interval("A4"));
+    }
+
+    #[test]
+    fn test_rand_semitone(){
+        for _ in 0..1000{
+            let semitone = rand_semitone();
+            assert!(semitone < 12, "Semitone {} out of range", semitone);
+        }
+    }
+
+    #[test]
+    fn test_create_interval_semitones(){
+        let interval = create_interval_semitones(7);
+        assert_eq!(7, interval.semitone_count);
+
+        let bad_interval = create_interval_semitones(13);
+        assert_eq!(0, bad_interval.semitone_count);
+    }
+
+    #[test]
+    fn test_create_interval_string(){
+        let interval = create_interval_string("P5");
+        assert_eq!(7, interval.semitone_count);
+
+        let interval = create_interval_string("d4");
+        assert_eq!(4, interval.semitone_count);
+
+        let interval = create_interval_string("p8");
+        assert_eq!(0, interval.semitone_count);
+    }
+
+    #[test]
+    fn test_create_rand_interval(){
+        let interval_result = create_rand_interval();
+        assert!(interval_result.is_ok());        
+    }
+
+    fn mock_create_rand_interval() -> Result<Interval, IntervalError> {
+        Interval::from_semitone(4)
+    }
+
+    fn mock_create_note_from_rand_interval(note: Note)-> (Note, Interval) {
+        let interval_result = mock_create_rand_interval();
+        let interval = match interval_result {
+            Ok(interval) => interval,
+            Err(_error) => Interval::default(),
+        };
+    
+        //println!("Random Interval is {:?}", interval);
+        let new_note = interval.second_note_from(note);
+        (new_note, interval)
+    }
+
+    #[test]
+    fn test_create_note_from_rand_interval(){
+        let pitch = Pitch::new(NoteLetter::B, 0);
+        let note = Note::new(pitch, 4);
+
+        let(note2, interval) = mock_create_note_from_rand_interval(note);
+
+        let note2_pitch = note2.pitch.into_u8();
+        assert_eq!(3, note2_pitch);
+        assert_eq!(4, interval.semitone_count);
+    }
+
+    #[test]
+    fn test_create_note_from_given_interval(){
+        let pitch = Pitch::new(NoteLetter::F, 0);
+        let root = Note::new(pitch, 4);
+        let interval = Interval::from_semitone(10).unwrap();
+        let note = create_note_from_given_interval(root, interval, &mut String::from("up"));
+
+        let note2_pitch = note.pitch.into_u8();
+        assert_eq!(3, note2_pitch);
+        assert_eq!(5, note.octave);
+    }
+}
